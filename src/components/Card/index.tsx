3134e3040412c73404093d3af8ea85838e7e73b3
nestonzin <dinestoor@outlook.com>
2022-08-25 20:58:11 -0300

    card c js

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,34 +1,44 @@
 import { Flex, Text, Box, Button } from "@chakra-ui/react";
 
 export const Card = () => {
+  const cards = [
+    { image: {}, title: "Title", description: "description", cta: "Veja mais" },
+    { image: {}, title: "Title", description: "description", cta: "Veja mais" },
+    { image: {}, title: "Title", description: "description", cta: "Veja mais" },
+    { image: {}, title: "Title", description: "description", cta: "Veja mais" },
+  ];
+
   return (
     <Flex
       padding={["2rem"]}
       gap="1rem"
-      flexDirection={["column", "column", "column", "row"]}
+      flexDirection={["column", "column", "column", "column"]}
       alignItems={"center"}
     >
-      <Flex
-        w={["15rem"]}
-        h={["20rem"]}
-        bg="blue"
-        justifyContent={["center"]}
-        p={["1rem"]}
-        flexDirection={["column"]}
-        alignItems={["center"]}
-        gap={[".5rem"]}
-        borderRadius={["1rem"]}
-      >
-        <Box w={["10rem"]} h={["5rem"]} bg="yellow" borderRadius={[".5rem"]}></Box>
-        <Text color={["black"]}>Title</Text>
-        <Text color={["black"]} textAlign={["center"]}>
-          description... bla bla bla bla bla bla bla bla bla bla bla bla bla bla
-          bla bla bla bla bla bla bla bla bla bla bla la
-        </Text>
-        <Button w="5rem" h="2.5rem">
-          Veja mais
-        </Button>
-      </Flex>
+      {" "}
+      {cards.map((card) => (
+        <Flex
+          w={["15rem"]}
+          h={["20rem"]}
+          bg="blue"
+          justifyContent={["center"]}
+          p={["1rem"]}
+          flexDirection={["column"]}
+          alignItems={["center"]}
+          gap={[".5rem"]}
+          borderRadius={["1rem"]}
+        >
+          <Box
+            w={["10rem"]}
+            h={["5rem"]}
+            bg="yellow"
+            borderRadius={[".5rem"]}
+          ></Box>
+          <Text color={["black"]}></Text>
+          <Text color={["black"]} textAlign={["center"]}></Text>
+          <Button w="5rem" h="2.5rem"></Button>
+        </Flex>
+      ))}
     </Flex>
   );
 };
