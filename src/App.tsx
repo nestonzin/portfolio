b41af9ba53162daa5b5ffd266f4b6ce520ef461d
nestonzin <dinestoor@outlook.com>
2022-08-25 23:32:45 -0300

    projects vira Projects

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/home";
-import { Projects } from "./pages/projects";
+import { Projects } from "./pages/Projects";
 
 
 function App() {
